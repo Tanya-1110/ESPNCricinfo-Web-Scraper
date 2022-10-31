1ad8404699a52f4bea428eb459a90255dea665bd
Tanya <goyaltanya2005@gmail.com>
2022-10-31 23:29:18 +0530

    Commit changed

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595";
+const fs = require("fs");
+const path = require("path");
 // Venue date opponent result runs balls fours sixes sr
 const request = require("request");
 const cheerio = require("cheerio");
 const AllMatcgObj = require("./Allmatch");
 // home page 
+const iplPath = path.join(__dirname, "ipl");
+dirCreater(iplPath);
 request(url, cb);
 function cb(err, response, html) {
     if (err) {
@@ -21,4 +25,10 @@ function extractLink(html) {
     let fullLink = "https://www.espncricinfo.com" + link;
     // console.log(fullLink);
     AllMatcgObj.gAlmatches(fullLink);
+}
+
+function dirCreater(filePath) {
+    if (fs.existsSync(filePath) == false) {
+        fs.mkdirSync(filePath);
+    }
 }
